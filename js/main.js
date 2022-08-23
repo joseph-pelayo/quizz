120d23aa756fa4198e5f5d28e94f4b7175112ecb
Joseph Pelayo <jeanjoseph.pelayo@gmail.com>
2022-08-23 15:42:02 +0400

    Modifications fichiers html et js + rajout fichier css

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Adresse du fichier de données json
+let url ="https://pierre-delaunay.fr/marvel.json";
+
+async function getData() {
+    // Déclaration d'un objet associé à la méthode fetch 
+    let response = await fetch(url);
+
+    // Teste la réponse du serveur
+    if(response.status === 200) {
+        let data = await response.json();
+
+        // console.log(data);
+        // console.log(data.difficulté);
+        // console.log(data.)
+
+        console.log(data.quizz.fr.débutant[1].question);
+    }
+}
+
+getData();
